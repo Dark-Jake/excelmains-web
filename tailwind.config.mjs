30873db4ef42098c696a2d81d605e273290eb2a9
DarkJake <thedark.tutos11@gmail.com>
2024-04-03 01:07:16 -0600

    Added Theme prefer and animated backgroud

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,22 @@
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '#333',
-				'secondary': '#ddd',
-				'youtube': '#de332f'
-			}
-		},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        youtube: "var(--color-youtube)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [
+    animations,
+    function ({ addVariant }) {
+      addVariant("any-hover", "@media (any-hover: hover) { &:hover }");
+    },
+  ],
+};
